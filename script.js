419a3170ca711a8c0c1a745732db22f4b0996d82
Paula Cassia Feltrin <feltrin.paulacassia@gmail.com>
2022-06-09 17:57:14 -0300

    Desafio completo

---
+++ script.js
@@ -0,0 +1,16 @@
+let fullName = "Paula Cassia Feltrin";
+let num = 57;
+let ano = 2022;
+let res = ano - num;
+let somaRes = ano + num;
+let frase = "Estou mexendo no console.log";
+let quartaFeira = false;
+
+console.log(`Meu nome é ${fullName}.`);
+console.log(`O número escolhido foi ${num}.`);
+console.log(`Estamos no ano de ${ano}.`);
+console.log(`${ano} - ${num} é igual a ${res}`);
+console.log(`${ano} + ${num} é igual a ${somaRes}`);
+console.log(`${frase}`);
+console.log(quartaFeira);
+console.log(`O tipo de dado da variável quartaFeira é ${typeof quartaFeira}.`);
